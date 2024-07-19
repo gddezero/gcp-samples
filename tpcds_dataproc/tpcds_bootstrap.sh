e073a3b8543434b9b785e3b495ec888e26e26229
Forrest Xi <maxwellx@google.com>
2024-07-19 16:30:09 +0800

    fix error in tpcds_bootstrap.sh

--- tpcds_dataproc/tpcds_bootstrap.sh
+++ tpcds_dataproc/tpcds_bootstrap.sh
@@ -5,5 +5,5 @@ git clone https://github.com/gddezero/gcp-samples.git
 cd gcp-samples/tpcds_dataproc
 tar -zxvf tpcds-kit.tar.gz
 ROOT_DIR=$(/usr/share/google/get_metadata_value attributes/ROOT_DIR)
-sed -i "s/ROOT_DIR/${ROOT_DIR}/g" datagen.scala
-sed -i "s/ROOT_DIR/${ROOT_DIR}/g" create_table.scala
+sed -i "s#<ROOT_DIR>#${ROOT_DIR}#g" datagen.scala
+sed -i "s#<ROOT_DIR>#${ROOT_DIR}#g" create_table.scala
